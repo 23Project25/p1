55773767ba3e00f62fb514114e730d2ad90ff623
23Project25 <abeshsarkar322@gmail.com>
2025-08-11 15:30:28 +0530

    b

--- myapp/src/Home.jsx
+++ myapp/src/Home.jsx
@@ -1,6 +1,22 @@
 
+<<<<<<< Updated upstream
 export default function Home(){
     return(
         <h1>This is our Home page</h1>
     );
+=======
+export default function Home()
+{
+    return (
+        <header>
+       <div className="logo">
+        <img src="">Logo</img>
+        <ul>
+            <li><link to="/">HOME</link></li>
+            <li><link to="/Login">LOG IN</link></li>
+        </ul>
+       </div>
+        </header>
+    )
+>>>>>>> Stashed changes
 }
--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+<<<<<<< Updated upstream
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+=======
+import Home from './Home.jsx'
+import Login from './Login.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Home/>
+    <Login/>
+>>>>>>> Stashed changes
   </StrictMode>,
 )
